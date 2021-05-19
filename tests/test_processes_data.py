dd13d3b5f36026f0b16d86e760b58efa777fc700
Luca Foresta <luca.foresta@eodc.eu>
2021-05-19 08:01:09 +0200

    create individual test for func(data, **kwargs) processes

--- tests/test_processes_data.py
+++ tests/test_processes_data.py
@@ -1,35 +1,35 @@
 """
+Tests processes with input type func(data, **kwargs)
 """
 
+from pytest import mark
 from openeo_odc.map_processes_odc import map_data
 
 
-def test_array_element():
+@mark.parametrize("node_id, process_def, kwargs, process_ref",
+    [
+        (
+            "nir_1",
+            {'process_id': 'array_element', 'arguments': {'data': 'dc_0', 'index': 0}},
+            {'from_parameter': {'data': 'dc_0'}, 'dimension': 'spectral'},
+            "nir_1 = oeop.array_element(**{'data': dc_0, 'index': 0, 'dimension': 'bands'})\n"
+        ),
+        (
+            "nir_2",
+            {'process_id': 'array_element', 'arguments': {'data': 'dc_0', 'label': 'nir'}},
+            {'from_parameter': {'data': 'dc_0'}, 'dimension': 'spectral'},
+            "nir_2 = oeop.array_element(**{'data': dc_0, 'label': 'nir', 'dimension': 'bands'})\n"
+        ),
+        (
+            "nir_3",
+            {'process_id': 'array_element', 'arguments': {'data': 'dc_0', 'label': 'nir', 'return_nodata': True}},
+            {'from_parameter': {'data': 'dc_0'}, 'dimension': 'spectral'},
+            "nir_3 = oeop.array_element(**{'data': dc_0, 'label': 'nir', 'return_nodata': True, 'dimension': 'bands'})\n"
+        ),
+    ]
+)
+def test_array_element(node_id, process_def, kwargs, process_ref):
     """Test all array_element conversions."""
 
-    process_defs = [
-        (
-         'nir_1',
-         {'process_id': 'array_element', 'arguments': {'data': 'dc_0', 'index': 0}},
-         {'from_parameter': {'data': 'dc_0'}, 'dimension': 'spectral'}
-        ),
-        (
-         'nir_2',
-         {'process_id': 'array_element', 'arguments': {'data': 'dc_0', 'label': 'nir'}},
-         {'from_parameter': {'data': 'dc_0'}, 'dimension': 'spectral'}
-        ),
-        (
-         'nir_3',
-         {'process_id': 'array_element', 'arguments': {'data': 'dc_0', 'label': 'nir', 'return_nodata': True}},
-         {'from_parameter': {'data': 'dc_0'}, 'dimension': 'spectral'}
-        )
-    ]
-    process_refs = [
-        "nir_1 = oeop.array_element(**{'data': dc_0, 'index': 0, 'dimension': 'bands'})\n",
-        "nir_2 = oeop.array_element(**{'data': dc_0, 'label': 'nir', 'dimension': 'bands'})\n",
-        "nir_3 = oeop.array_element(**{'data': dc_0, 'label': 'nir', 'return_nodata': True, 'dimension': 'bands'})\n"
-    ]
-
-    for k, process_def in enumerate(process_defs):
-        out = map_data(process_def[0], process_def[1], process_def[2])
-        assert out == process_refs[k]
+    out = map_data(node_id, process_def, kwargs)
+    assert out == process_ref
