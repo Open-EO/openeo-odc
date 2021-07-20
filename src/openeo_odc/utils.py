cb00d4d33fe30c908544d0f3b0e56eec3784aa1e
sherrmann <sophie.herrmann@eodc.eu>
2021-07-20 16:04:25 +0200

    Add support for suffixed processes.
    Fixes #12
    
    
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

--- src/openeo_odc/utils.py
+++ src/openeo_odc/utils.py
@@ -8,3 +8,25 @@ class FitCurveUtils:
 
     def get_func_header(self, fc_node_id) -> str:
         return f"\n\ndef {self.get_func_name(fc_node_id)}(x, *parameters):\n"
+
+
+SUFFIXED_PROCESSES = [
+    "and",
+    "any",
+    "all",
+    "if",
+    "or",
+]
+
+def get_py_process_name(process: str) -> str:
+    """Return correct process name as in `openeo-processes-python` repo."""
+    if process in SUFFIXED_PROCESSES:
+        process += "_"
+    return process
+
+
+def get_oeop_str(id, process_name, params_str) -> str:
+    """Return `openeo-processes-python` call."""
+    oeop_name = get_py_process_name(process_name)
+    return f"""{'_'+id} = oeop.{oeop_name}({params_str})
+"""
