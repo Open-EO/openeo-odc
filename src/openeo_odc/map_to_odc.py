9858956252219b446b4d8acf2d6af9ff590fa5c6
sean <sean.hoyal@eodc.eu>
2022-03-11 10:37:04 +0100

    apapt range 1-3

--- src/openeo_odc/map_to_odc.py
+++ src/openeo_odc/map_to_odc.py
@@ -132,7 +132,7 @@ options = gateway.cluster_options()
 options.user_id = '{user_id}'
 options.job_id = '{job_id}'
 cluster = gateway.new_cluster(options)
-cluster.adapt(minimum=1, maximum=4)
+cluster.adapt(minimum=1, maximum=3)
 time.sleep(60)
 client = cluster.get_client()
 """
